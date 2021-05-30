f5b5b81b3a4390c08374bda131376e9afac87050
Chandan Kumar <47857139+chandan73933@users.noreply.github.com>
2021-05-30 16:01:55 +0530

    Add files via upload

---
+++ template.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+template<class T>
+void display(Tx)
+{
+	cout<<""<<x;
+}
+template<class T,class T1>
+void display(Tx,T1y){
+	cout<<x
+	cout<<y;
+}
+void dispaly(int x)
+{
+	cout<<x<<"\n";
+}
+int main()
+{
+	
+	dispaly(100);
+	dispaly(12.34);
+	dispaly(100,12.34);
+	dispaly('c');
+	
+	
+	
+}
